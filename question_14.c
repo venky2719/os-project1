88d8825531aea459bdb483a562e87d5b6765f2b1
venky2719 <38438933+venky2719@users.noreply.github.com>
2018-04-23 13:07:46 +0530

    Add files via upload

---
+++ question_14.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main(){
+	int i,j,n,distance=0;
+	printf("\Enter the total number of requests: \n");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter the values of requests: \n");
+	for(i=0;i<n;i++)
+	{
+	scanf("%d",&arr[i]);
+	}
+	int hs,pv;
+	printf("enter the head start(current position) value: ");
+	scanf("%d",&hs);
+	printf("enter the previous value of head start:");
+	scanf("%d",&pv);
+	 if(hs<pv){
+     	distance=distance+(pv-hs);
+	 }
+	 else{
+	 	distance=distance+(hs-pv);
+	 }
+	
+	for(i=0;i<n-1;i++)
+	{
+		if(arr[i] < arr[i+1])
+		distance += arr[i+1] - arr[i];
+		else
+		distance += arr[i] - arr[i+1];
+    }
+    if(arr[0]<pv){
+     	distance=distance+(pv-arr[0]);
+	 }
+	 else{
+	 	distance=distance+(arr[0]-pv);
+	 }
+     
+	printf("\nTotal distance(head movement) by drive is: %d",distance);
+}
